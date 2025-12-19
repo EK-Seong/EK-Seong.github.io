11d1b701b55fe190770a3d5e8eb93f1c037018fb
EK-Seong <131163793+EK-Seong@users.noreply.github.com>
2025-12-19 16:02:58 +0000

    Deploying to gh-pages from @ EK-Seong/EK-Seong.github.io@d68507a0fbc70cbf6b798b311325576d2351ed6b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -74,6 +74,13 @@ ninja.data = [{
           window.open("/feed.xml", "_blank");
         },
       },{
+        id: 'social-scholar',
+        title: 'Google Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://scholar.google.com/citations?user=djaDDCgAAAAJ", "_blank");
+        },
+      },{
         id: 'social-x',
         title: 'X',
         section: 'Socials',
